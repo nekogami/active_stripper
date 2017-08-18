61c9d8c4c021765f4bc1e77b912182f01415027a
François TCHENG <tcheng.f@gmail.com>
2017-08-18 13:44:57 +0200

    Fix use of method defined in including class to process value by passing a reference to the class

--- lib/active_stripper.rb
+++ lib/active_stripper.rb
@@ -12,15 +12,16 @@ module ActiveStripper # Pun intended
   #
   # @param [String] val String to manipulate
   # @param [Hash/Array] iterable_operators Methods details to use on `val`
+  # @param [Object] base Instance including the current module
   #
   # @return [String] Modified String
   #
-  def self.iterable_apply(val, iterable_operators)
+  def self.iterable_apply(val, iterable_operators, base)
     iterable_operators.each do | operator_name, operator_args |
 
       val = case
             when !operator_args
-              ActiveStripper.method_apply(val, operator_name)
+              ActiveStripper.method_apply(val, operator_name, base)
             when operator_args[:module]
               Module.const_get(operator_args[:module])
                     .send(operator_name, val)
@@ -36,15 +37,16 @@ module ActiveStripper # Pun intended
   #
   # @param [String] val Value to modify
   # @param [String/Symbol] operator Method name to execute
+  # @param [Object] base Instance including the current module
   #
   # @return [String] Modifies value
   #
-  def self.method_apply(val, operator)
+  def self.method_apply(val, operator, base)
     case
     when ActiveStripper::Helpers.respond_to?(operator)
       ActiveStripper::Helpers.send(operator, val)
-    when respond_to?(operator)
-      send(operator, val)
+    when base.respond_to?(operator)
+      base.send(operator, val)
     else
       val
     end
@@ -74,9 +76,9 @@ module ActiveStripper # Pun intended
                   when "Proc"
                     operator.call(val)
                   when "Hash", "Array"
-                    ActiveStripper.iterable_apply(val, operator)
+                    ActiveStripper.iterable_apply(val, operator, self)
                   when "String", "Symbol"
-                    ActiveStripper.method_apply(val, operator)
+                    ActiveStripper.method_apply(val, operator, self)
                   end
 
             super(val)
