2d45bf84e10ef8f45db39527bc73889be76f7258
François TCHENG <tcheng.f@gmail.com>
2017-08-23 16:00:18 +0200

    Added basic test for Main module method #strip_value_from

--- spec/active_stripper_spec.rb
+++ spec/active_stripper_spec.rb
@@ -1,11 +1,97 @@
 require 'spec_helper'
 
 describe ActiveStripper do
-  it 'has a version number' do
-    expect(ActiveStripper::VERSION).not_to be nil
+
+  before do
+    stub_const("TEST_STRING", "    Test  String  ")
+    stub_const("Bar", Class.new)
+    Bar.class_eval do
+      include ActiveStripper
+
+      attr_accessor :test_accessor
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context "When included in an subclass" do
+    let(:foo) do
+      foo = Bar.new
+      foo.test_accessor = TEST_STRING
+      foo
+    end
+
+    context "#strip_value_from(:test_accessor, :to_lower_stripper) execution make it so that" do
+      before do
+        Bar.class_eval do
+          strip_value_from(:test_accessor, :to_lower_stripper)
+        end
+      end
+
+      context "#test_accessor" do
+        it { expect(foo.test_accessor).to eql foo.test_accessor.downcase }
+      end
+    end
+
+    context "#strip_value_from(:test_accessor, [:to_lower_stripper, :white_space_stripper]) execution make it so that" do
+      before do
+        Bar.class_eval do
+          strip_value_from(:test_accessor, [:to_lower_stripper, :white_space_stripper])
+        end
+      end
+
+      context "#test_accessor" do
+        it { expect(foo.test_accessor).to eql foo.test_accessor.downcase.strip }
+      end
+    end
+
+    context "#strip_value_from(:test_accessor, processor: { module: :TestModule }) execution make it so that" do
+      before do
+        stub_const("CustomModule", Module.new)
+        CustomModule.module_eval do
+          def self.processor(val)
+            return val.to_s + " curstom processor executed"
+          end
+        end
+
+        Bar.class_eval do
+          strip_value_from(:test_accessor, processor: { module: :CustomModule } )
+        end
+      end
+
+      context "#test_accessor" do
+        it { expect(foo.test_accessor).to eql CustomModule.processor(TEST_STRING) }
+      end
+    end
+
+    context "#strip_value_from(:test_accessor, -> (val) { val + ' lambda processor' }) execution make it so that" do
+      let(:lambda_method) { -> (val) { val + ' lambda processor' } }
+
+      before do
+        Bar.class_eval do
+          strip_value_from(:test_accessor, -> (val) { val + ' lambda processor' })
+        end
+      end
+
+      context "#test_accessor" do
+        it { expect(foo.test_accessor).to eql lambda_method.call(TEST_STRING) }
+      end
+    end
+
+    context "with custom setter defined in the class" do
+      context "#strip_value_from(:test_accessor, :to_lower_stripper) execution make it so that" do
+        before do
+          Bar.class_eval do
+            strip_value_from(:test_accessor, :to_lower_stripper)
+
+            def test_accessor=(val)
+              @test_accessor = val + " custom setter"
+            end
+          end
+        end
+
+        context "#test_accessor" do
+          it { expect(foo.test_accessor).to eql TEST_STRING.downcase + " custom setter" }
+        end
+      end
+    end
   end
 end
