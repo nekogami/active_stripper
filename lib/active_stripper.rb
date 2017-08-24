cdfc060dc4369bd2cb0f5f2eddc00fd07af666ef
François TCHENG <tcheng.f@gmail.com>
2017-08-24 17:28:09 +0200

    [f#1] Changed base code to pass additional args to processor
      - Changed #method_apply prototype to receive a module_name for lookup and optionnal additional args
      - Changed how we iterate in #iterable_apply to reuse the new prototype of #method_apply
      - Added a way for the user to force lookup in the inclusion object instead of ActiveStripper::Helpers in case of name collision

--- lib/active_stripper.rb
+++ lib/active_stripper.rb
@@ -21,36 +21,50 @@ module ActiveStripper # Pun intended
   def self.iterable_apply(val, iterable_operators, base)
     iterable_operators.each do | operator_name, operator_args |
 
-      val = case
-            when !operator_args
-              ActiveStripper.method_apply(val, operator_name, base)
-            when operator_args[:module]
-              Module.const_get(operator_args[:module])
-                    .send(operator_name, val)
-            end
+      additionnal_args = nil
+      lookup_module_name = "ActiveStripper::Helpers"
+
+      if operator_args
+        additionnal_args = operator_args[:additionnal_args]
+        lookup_module_name = operator_args[:module] if operator_args[:module]
+      end
+
+      val = ActiveStripper.method_apply(val,
+                                        operator_name,
+                                        base,
+                                        lookup_module_name,
+                                        additionnal_args)
     end
 
     return val
   end
 
   #
-  # Look up for method `operator` in ActiveStripper::Helpers and current
-  # class and call it
+  # Look up for method `operator` in ActiveStripper::Helpers THEN in current
+  # class if not found and call it
   #
-  # @param [String] val Value to modify
-  # @param [String/Symbol] operator Method name to execute
-  # @param [Object] base Instance including the current module
+  # @param [String] val         Value to modify
+  # @param [Symbol] operator    Method name to execute
+  # @param [Object] base        Instance including the current module
+  # @param [String] module_name Name of the module where we are going to lookup
+  #                             a method which name is contained in `operator`
+  #                             if equal to EMPTY STRING the lookup context will
+  #                             be the object passed through `base`
+  # @param [Array]  args        (Optional)
+  #                             Additionnal args to pass to the processor method
   #
-  # @return [String] Modifies value
+  # @return [String] Processed val if possible otherwise return val is it was
   #
-  def self.method_apply(val, operator, base)
+  def self.method_apply(val, operator, base, module_name, args = nil)
+    lookup_module = (module_name == "") ? base : Module.const_get(module_name)
+
     case
     when operator.class.name === "Proc"
-      operator.call(val)
-    when ActiveStripper::Helpers.respond_to?(operator)
-      ActiveStripper::Helpers.send(operator, val)
-    when base.respond_to?(operator)
-      base.send(operator, val)
+      operator.call(val, *args)
+    when lookup_module.respond_to?(operator)
+      lookup_module.send(operator, val, *args)
+    when lookup_module != base && base.respond_to?(operator)
+      base.send(operator, val, *args)
     else
       val
     end
@@ -72,9 +86,9 @@ module ActiveStripper # Pun intended
       args = args.reject { | field | self.respond_to?(field) }
 
       # Dynamically generate an anonymous module to be prepended
+      # self is equal to the object where the current method is called
       mod = Module.new do
         args.each do | field |
-
           define_method :"#{field}=" do | val |
             val = case operator.class.name
                   when "Proc"
@@ -82,7 +96,10 @@ module ActiveStripper # Pun intended
                   when "Hash", "Array"
                     ActiveStripper.iterable_apply(val, operator, self)
                   when "String", "Symbol"
-                    ActiveStripper.method_apply(val, operator, self)
+                    ActiveStripper.method_apply(val,
+                                                operator,
+                                                self,
+                                                "ActiveStripper::Helpers")
                   end
 
             super(val)
